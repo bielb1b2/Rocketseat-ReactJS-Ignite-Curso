5392bbf3d49ddc2c6828630d3f5c16f1aea1c059
Gabriel Souza <gabrielvt.souza@hotmail.com>
2021-04-16 15:31:53 -0300

    Contexto e Hooks

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionProvider } from './hooks/useTransactions';
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root');
@@ -20,7 +21,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTrasactionModal}/>
       
       
@@ -33,6 +34,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionProvider>
   );
 }
